006a82fa7c19ab81d543558c7a889404a769d279
bryfox <bryan@bryfox.com>
2012-01-14 16:06:12 -0800

    More outlining work for sections 6 and 7; updates and better commenting in JS for JQM app

--- exercises/06-band-of-brothers/02-local-storage/zombie.js
+++ exercises/06-band-of-brothers/02-local-storage/zombie.js
@@ -1,6 +1,7 @@
-window.zombieManager = function() {
+window.zombieManager = (function() {
   var zombies       = new Array();
   var zombieChair   = Lawnchair({name: 'zombies'}, function() {});
+  // Each killing method has a corresponding icon. Here we map them.
   var zombie_icons  = {
     'nukes'         : 'z2.png',
     'conflagration' : 'z5.png',
@@ -10,21 +11,27 @@ window.zombieManager = function() {
     'corrosive'     : 'z8.png',
     'dismemberment' : 'z14.png'
   };
-  var $zombieList = $('#zombielist');
-
+  // Wipe out the zombies and start over
   var resetZombies = function() {
     zombies = new Array();
     zombieChair.nuke();
     $('li.zombie').remove();
-    $zombieList.listview('refresh');
+    $('#zombielist').listview('refresh');
   };
   
+  // Refresh/show <ul> of zombies nabbed
   var showZombies = function() {
     for(var i = 0; i < zombies.length; i++) {
       var zombieWhen = new Date(zombies[i].when);
+      // Formatting the date-time using the format JS library
+      // Generate a unique-ish ID for the Zombie based on the datetime
+      // it was nabbed.
       var zombieID   = 'zombie-' + zombieWhen.format('mdyhis');
+      // If there isn't already a row for this Zombie ID
       if (!$("#" + zombieID).length) {
+        // Create a new <li> element and populate it with info about the zombie
         var $zombieRow = $('<li></li>').addClass('zombie').attr('id', zombieID);
+        // Add an appropriate icon image for the killing method
         if (zombies[i].how && zombie_icons[zombies[i].how]) {
           $zombieImg = $("<img>").attr(
             { src :  'icons/' + zombie_icons[zombies[i].how],
@@ -33,27 +40,40 @@ window.zombieManager = function() {
           $zombieRow.append($zombieImg);
         }
         $zombieRow.append( 
-          '<h3>Zombie Nabbed!</h3><p><strong>' + zombieWhen.format('m/d/y H:i') + '</strong></p>'
+          '<h3>Zombie Nabbed!</h3><p><strong>' 
+          + zombieWhen.format('m/d/y H:i') 
+          + '</strong></p>'
         );
+        // If there is location information, add it.
         if (zombies[i].where && zombies[i].where.coords) {
           var coords = zombies[i].where.coords;
-          $zombieRow.append('<p>Coordinates: ' + coords.latitude + ', ' + coords.longitude + '</p>');
+          $zombieRow.append('<p>Coordinates: ' 
+          + coords.latitude + ', ' 
+          + coords.longitude + '</p>');
         }
+        // Prepend the new <li> to the beginning of #zombielist
         $('#zombielist').prepend($zombieRow);
         
       }
     }
+    // Add a reset button if one is not there already (and there is 
+    // at least one zombie)
     if (zombies && zombies.length && !$('#zombie-reset').length) {
       $startOver = $('<button>Reset my Zombies</button>').attr('id', 'zombie-reset');
       $('#zombielist').after($startOver);
+      // Assign click handler to reset the zombie list
       $('#zombie-reset').button().click(function() {
         resetZombies();
       });
     }
+    // Refresh the listview for #zombielist because we've (likely)
+    // altered the list. TODO: Use variable instead of ID here.
     $('#zombielist').listview('refresh');
   };
   
+  // Return object literal with two available methods (init and addZombie)
   return {
+    // Init; build initial #zombielist
     init          : function() {
       zombieChair.get('zombies', function(zombiesOnIce) {
         if (zombiesOnIce) {
@@ -62,6 +82,7 @@ window.zombieManager = function() {
       });
       showZombies();
     },
+    // Add a new zombie to the list of zombies
     addZombie     : function(zombieWhen, zombieWhere, zombieHow) {
       zombies[zombies.length] = {
         when      : zombieWhen.valueOf(),
@@ -72,32 +93,30 @@ window.zombieManager = function() {
       showZombies();
     }
   }
-}();
+}());
 
+// TODO: Crap. Should this be pagecreate?
 $("#nab-zombie").live('pageinit', function() {
 
+  // Initialize our list of zombies
   zombieManager.init();
-  
-  var geoHappy = false;
-  if (geo_position_js.init()) {
-    geoHappy = true;
-  }
+  // Is Geolocation supported?
+  var geoHappy = geo_position_js.init();
+  // Click handler for the "Got One" button
   $("#got-one").click(function() {
     var now = new Date();
     var how = $("#how-zombie").val();
+    // Get position data if supported
     if (geoHappy) {
       geo_position_js.getCurrentPosition(function(position) {
         zombieManager.addZombie(now, position, how);
       }, function(error) {
+        console.log("Geolocation error: " + error.message)
         zombieManager.addZombie(now, null, how);
       });
-    } else {
+    } else { // It's also OK to add a zombie w/o location data
       zombieManager.addZombie(now, null, how);
     }
-  });
-  var $viewportMeta = $('meta[name="viewport"]');
-  $('input, select, textarea').bind('focus blur', function(event) {
-    $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
   });
 });
 
