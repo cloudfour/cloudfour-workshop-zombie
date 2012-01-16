d0d5669162f53e449abe2863bc9cd9514f49b496
Lyza Danger Gardner <lyza@lyza.com>
2012-01-16 08:17:30 -0800

    Performance updates to build script

--- bin/build.pl
+++ bin/build.pl
@@ -9,6 +9,11 @@ use File::stat;
 use File::Basename;
 use Getopt::Std;
 
+getopts("hfvr:", \%opts);
+if ($opts{h}) {
+  usage();
+}
+
 ## Settings for this project
 my $pandoc = '/usr/local/bin/pandoc';
 
@@ -24,33 +29,95 @@ $src_path     = $basepath . "/assets";
 $pack_path    = $basepath . "/zombie-rucksack";
 $ex_path      = $basepath . "/exercises";
 
+## When was the last time a shared/common resource was updated?
+my $common_mtime = 0;
+my @common_dirs = (
+  "$basepath/assets",
+  "$content_path/images"
+);
+# What's the newest mtime on the source HTML?
+my $now = time();
+find( sub {
+  my $mtime = stat($_)->mtime;
+  if ($mtime > $common_mtime) {
+    $common_mtime = $mtime;
+  }
+}, @common_dirs);
+
 ## Generate outline in slidy and html form
 ## more later...
-
-$src_file = $content_path . "/outline.mdown";
-$output   = $slide_path   . "/outline.html";
-$flags    = '--from=markdown --to=slidy --standalone';
-system("$pandoc $flags --css=../assets/styles/workshop.css $src_file > $output");
-
 
 sub build_presentation {
   if ($_ =~ /\.content/ && $File::Find::name !~ /field-notes/ ) {
     ($base, $dir, $ext) = fileparse($File::Find::name, '\..*'); # Split path into dir, file basename, extension
     my $pageid      = $base; # The page ID
-    my $output_path = $slide_path . '/' . $base . '.html';
-    system("$pandoc $flags --section-divs --css=../assets/styles/workshop.css $_ > $output_path");
+    my $output = $slide_path . '/' . $base . '.html';
+    my $test_update = needs_update($_, $output);
+    if ($test_update) {
+      system("$pandoc $flags --section-divs --css=../assets/styles/workshop.css $_ > $output");
+      if ($opts{v}) {
+        print "Updated $_ --> $output \n";
+      }
+    }
+    elsif ($opts{v}) {
+      print "$_ not updated \n";
+    }
   }
+}
+
+sub needs_update {
+  my $needs_update    = 0;
+  my $content         = $_[0];
+  my $output          = $_[1];
+  if ($opts{f} || ! -e $output) { # if force-build or output doesn't exist
+    $needs_update     = 1;
+  }
+  else {
+    my $content_mtime = stat($content)->mtime;
+    my $output_mtime  = stat($output)->mtime;
+    # Is output older than the content?
+    if ($output_mtime < $content_mtime) {
+      if ($opts{v}) {
+        print "$output needs update because content is changed \n";
+      }
+      $needs_update = 1;
+    }
+    elsif ($output_mtime < $common_mtime) { # Is output older than a change to a common source file?
+      if ($opts{v}) {
+        print "$output needs update because common file has changed \n";
+      }
+      $needs_update = 1;
+    }
+  }
+  return ($needs_update);
 }
 
 sub build_survival {
   ($base, $dir, $ext) = fileparse($File::Find::name, '\..*'); # Split path into dir, file basename, extension
   my $output          = $pack_path . "/survival-tools/" . $base . ".html";
+  my $needs_update    = 0;
   my $css             = "fieldnotes.css";
   my $flags           = "--from=markdown --to=html --html5 --section-divs --standalone";
-  system("$pandoc $flags --css=$css $_ > $output"); 
+  if (&needs_update($_, $output)) {
+    system("$pandoc $flags --css=$css $_ > $output"); 
+    print "$_ updated --> $output \n";
+  }
+}
+
+sub usage {
+  print  << "EOF";
+  
+  Usage: $0 [-vf]
+  
+  -f        : force full site rebuild, even if no recent changes detected
+  -v        : verbose output
+EOF
+
+    exit;
 }
 
 my @dirlist = $content_path;
+
 find(\&build_presentation, @dirlist);
 
 # Build field notes and other rucksack items
@@ -58,3 +125,11 @@ my @survival_items = ($content_path . "/field-notes.content",
                       $ex_path . "/03-release-hounds/01-going-fluid/fluid-css-changes.content",
                       $ex_path . "/03-release-hounds/02-media-queries/sigma-media-query.content");
 find(\&build_survival, @survival_items);
+
+$src_file = $content_path . "/outline.mdown";
+$output   = $slide_path   . "/outline.html";
+$flags    = '--from=markdown --to=slidy --standalone';
+if (&needs_update($src_file, $output)) {
+  system("$pandoc $flags --css=../assets/styles/workshop.css $src_file > $output");
+  print "Outline updated \n";
+}
