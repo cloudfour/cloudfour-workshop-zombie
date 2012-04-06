9c8b001f2be2c7a02e18a80214c1e668616286d3
Lyza Danger Gardner <lyza@lyza.com>
2012-04-06 10:10:30 -0700

    More tweaks to field notes and JS

--- rucksack/field-notes/fieldnotes.js
+++ rucksack/field-notes/fieldnotes.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("section[id^='snippet']").add("section[id^='some-details']").each(function() {
+    $(this).find('h3 a, h4 a, h5 a').attr('href', '');
     $(this).children('section').hide();
     $(this).addClass('snippet');
-    $(this).click(function() {
-      $(this).children('section').toggle();
-      $(this).toggleClass('open');
+    $(this).find('h3:first, h3 a:first, h4:first, h4 a:first, h2:first, h2 a:first').click(function(event) {
+      event.preventDefault();
+      $(this).parents('.snippet').children('section').toggle();
+      $(this).parents('.snippet').toggleClass('open');
     });
   });
 });
